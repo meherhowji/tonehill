d26b6a3a440e258eb1593eabe849fabbe23eaf2d
Meher-MihikaMacAir <hmeherranjan@gmail.com>
2023-08-26 23:03:01 +0530

    added other essential

--- App.tsx
+++ App.tsx
@@ -1,118 +1,150 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+/* eslint-disable react-native/no-inline-styles */
+import * as React from 'react';
+import {Dimensions} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import {PitchDetector} from 'react-native-pitch-detector';
+import {LineChart} from 'react-native-chart-kit';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export default function App() {
+  const [data, setData] = React.useState({tone: '--', frequency: 0});
+  const [chartData, setChartData] = React.useState([{value: 0, timestamp: 0}]);
+  const [isRecording, setIsRecording] = React.useState(false);
+  const [counter, setCounter] = React.useState(0);
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const start = async () => {
+    await PitchDetector.start();
+    const status = await PitchDetector.isRecording();
+    setIsRecording(status);
+  };
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const stop = async () => {
+    await PitchDetector.stop();
+    const status = await PitchDetector.isRecording();
+    setIsRecording(status);
+  };
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  React.useEffect(() => {
+    PitchDetector.addListener(setData);
+    return () => {
+      PitchDetector.removeListener();
+    };
+  }, []);
+
+  React.useEffect(() => {
+    if (typeof data.frequency === 'number') {
+      let newData, nextData;
+
+      if (chartData.length > 50) {
+        newData = [...chartData];
+        newData.shift();
+        nextData = [...newData, {value: data.frequency.toFixed(3), counter}];
+      } else {
+        nextData = [...chartData, {value: data.frequency.toFixed(3), counter}];
+      }
+
+      setChartData(nextData);
+      setCounter(counter + 1);
+    }
+  }, [data]);
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
+    <View style={styles.container}>
+      <Text style={styles.tone}>{data?.tone}</Text>
+      <Text style={styles.frequency}>{data?.frequency?.toFixed(1)}hz</Text>
+      <Text style={[styles.status, {color: isRecording ? 'green' : 'red'}]}>
+        {isRecording ? 'ON' : 'OFF'}
       </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
+      <TouchableOpacity
+        style={styles.button}
+        onPress={isRecording ? stop : start}>
+        <Text style={styles.label}>{isRecording ? 'STOP' : 'START'}</Text>
+      </TouchableOpacity>
+      <LineChart
+        data={{
+          labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+          datasets: [
+            {
+              data: [
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+              ],
+            },
+          ],
+        }}
+        width={Dimensions.get('window').width} // from react-native
+        height={220}
+        yAxisLabel="$"
+        yAxisSuffix="k"
+        yAxisInterval={1} // optional, defaults to 1
+        chartConfig={{
+          backgroundColor: '#e26a00',
+          backgroundGradientFrom: '#fb8c00',
+          backgroundGradientTo: '#ffa726',
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16,
           },
-        ]}>
-        {children}
-      </Text>
+          propsForDots: {
+            r: '6',
+            strokeWidth: '2',
+            stroke: '#ffa726',
+          },
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+      />
     </View>
   );
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  tone: {
+    fontSize: 40,
+    fontColor: 'black',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+
+  frequency: {
+    fontSize: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+
+  button: {
+    marginTop: 20,
+    backgroundColor: 'black',
+    width: '50%',
+    minHeight: 50,
+    borderRadius: 100,
+    justifyContent: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+
+  label: {
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+
+  status: {
+    marginTop: 16,
+    color: 'black',
+  },
+
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
   },
 });
-
-export default App;
